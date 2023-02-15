397fb108a86e4e8073af510fc46392f5ac6c57fe
Paul Solomon <paultsolomon@gmail.com>
2023-02-15 10:34:56 +0000

    nesting

--- functions.py
+++ functions.py
@@ -11,17 +11,18 @@ def calculateTax(amount):
     return totalAmount
 
 
-def readFile():
+def readFile(userName):
     #print("This is the beginning of the file handling process")
     file1 = open("file1.txt", "r")
+    print("Thank you " + str(userName) + " for your patronage")
     print(file1.read())
     file1.close()
 
 
-def writeFile(totalAmount):
+def writeFile(totalAmount, userName):
     try:
         file2 =open("file2.txt", "w")
-        file2.write("Total amount for this customer is " + str(totalAmount) )
+        file2.write("Total amount for " +str(userName) + " is " + str(totalAmount) )
         file2.close()
     except file2.exception:
         print("There is an error with the file")
--- test.py
+++ test.py
@@ -4,10 +4,12 @@ import math
 
 #@TODO: Refactor code to also accept user name.
 
+userName = str(input('Please username: '))
 userInput = int(input('Please amount: '))
+
 bill = calculateTax(userInput)
 print("Total amount to be paid is: " , bill)
 print("This is the floored bill " , math.floor(bill))
-readFile()
-writeFile(bill)
+readFile(userName)
+writeFile(bill, userName)
 currentWorkingDirectory()
