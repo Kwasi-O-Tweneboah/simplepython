2c47f60ff7b9170b886fd805bf053d1e1be928f5
Kwasi O-T <98654769+Kwasi-O-Tweneboah@users.noreply.github.com>
2023-02-15 10:53:40 +0000

    made correction to the code

--- functions.py
+++ functions.py
@@ -11,17 +11,18 @@ def calculateTax(amount):
     return totalAmount
 
 
-def readFile():
+def readFile(inputName):
     #print("This is the beginning of the file handling process")
     file1 = open("file1.txt", "r")
+    print("Thsnk you " + str(inputName) + " for your patronage")
     print(file1.read())
     file1.close()
 
 
-def writeFile(totalAmount):
+def writeFile(inputName, totalAmount):
     try:
         file2 =open("file2.txt", "w")
-        file2.write("Total amount for this customer is " + str(totalAmount) )
+        file2.write("Total amount for " + str(inputName) + " is " + str(totalAmount))
         file2.close()
     except file2.exception:
         print("There is an error with the file")
--- test.py
+++ test.py
@@ -4,10 +4,11 @@ import math
 
 #@TODO: Refactor code to also accept user name.
 
+inputName = str(input('Please enter your name: '))
 userInput = int(input('Please amount: '))
 bill = calculateTax(userInput)
 print("Total amount to be paid is: " , bill)
 print("This is the floored bill " , math.floor(bill))
-readFile()
-writeFile(bill)
+readFile(inputName)
+writeFile(inputName, bill)
 currentWorkingDirectory()
